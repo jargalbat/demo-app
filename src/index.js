ab6488dcd05656d209b0d1f9b4e64c1c6b0127f1
jargalbat <jargalbat@meditech.world>
2022-06-22 16:24:09 +0800

    added mini drawer

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
+import MiniDrawer from "./pages/MiniDrawer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />}>
-      <Route index element={<Home />} />
-      <Route path="blogs" element={<Blogs />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="*" element={<NoPage />} />
-    </Route>
-  </Routes>
-</BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="blogs" element={<Blogs />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="minidrawer" element={<MiniDrawer />} />
+        <Route path="*" element={<NoPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
